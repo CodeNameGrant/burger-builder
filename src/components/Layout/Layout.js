595c6e9b0722021ab43c77b56eb400f73f04eaf5
GrantW <Grant.Walker@ksdc.co.za>
2020-08-27 13:50:59 +0200

    126, 127; Add Dynamic Ingredient Component and Prop Type Validation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,18 +2,21 @@ import React from 'react'
 
 import Aux from '../../hoc/Auxiliary'
 
-import styles from './Layout.module.css'
+import classes from './Layout.module.css'
 
-const layout = (props) => (
-  <Aux>
-    <div>
-      Toolbar, SideDraw, BackDrop
-    </div>
+export default function Layout( props ) {
+  return (
+    <Aux>
+      <div>
+        Toolbar, SideDraw, BackDrop
+      </div>
 
-    <main className={styles.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+      <main className={classes.Content}>
+        {props.children}
+      </main>
+    </Aux>
+  )
+}
 
-export default layout;
+
+
