75ec9020fa959cb58ffcbc55c919919cd2fb6695
GrantW <Grant.Walker@ksdc.co.za>
2020-08-27 14:53:17 +0200

    129; Output Burger Ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 import classes from './Burger.module.css'
 
-export default function Burger() {
+export default function Burger( props ) {
+  const transformedIngredients = Object.keys(props.ingredients)  // Extract keys from ingredients object, which are the types
+    .map((igType) => {
+      return [...Array(props.ingredients[igType])] // return array with a length the same as the ingredient type
+        .map((_, idx) => {  // Map each array item to a BurgerIngredient for that type
+          // key = 'meat1, meat2, etc'
+          return <BurgerIngredient key={igType + idx} type={igType} />
+        });
+    })
+
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom' />
     </div>
   )
