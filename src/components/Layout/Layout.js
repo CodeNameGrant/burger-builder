a5a9508b4182c1018bca3f67dd23399121dac70b
GrantW <Grant.Walker@ksdc.co.za>
2020-08-27 13:12:12 +0200

    124; Create Layout Component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Aux from '../../hoc/Auxiliary'
+
+const layout = (props) => (
+  <Aux>
+    <div>
+      Toolbar, SideDraw, BackDrop
+    </div>
+
+    <main>
+      {props.children}
+    </main>
+  </Aux>
+);
+
+export default layout;
