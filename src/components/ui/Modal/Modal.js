3ddb4e14695f12ab00fa6046474fcbcb35764296
GrantW <Grant.Walker@ksdc.co.za>
2020-08-27 20:09:59 +0200

    139; Implementing the Backdrop Component

--- src/components/ui/Modal/Modal.js
+++ src/components/ui/Modal/Modal.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
+import Aux from '../../../hoc/Auxiliary';
+import Backdrop from '../Backdrop/Backdrop';
+
 import classes from './Modal.module.css';
 
 export default function Model( props ) {
   return (
-    <div className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0',
-      }}>
-      {props.children}
-    </div>
+    <Aux>
+      <Backdrop show={props.show} click={props.modalClosed}/>
+      <div className={classes.Modal}
+        style={{
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0',
+        }}>
+        {props.children}
+      </div>
+    </Aux>
   )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -112,6 +112,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: true });
   }
 
+  purchaseCancelledHandler = () => {
+    this.setState({ purchasing: false });
+  }
+
   /*
     !!! IMPORTANT !!!
     myFunc() {
@@ -134,7 +138,7 @@ class BurgerBuilder extends Component {
 
     return (
         <Aux>
-          <Modal show={this.state.purchasing}>
+          <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelledHandler}>
             <OrderSummary ingredients={this.state.ingredients} />
           </Modal>
 
