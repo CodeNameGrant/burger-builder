9233a7821f2032103f2c82adc3fee69d01665588
GrantW <Grant.Walker@ksdc.co.za>
2020-09-01 17:37:40 +0200

    231; Outputting the Orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,22 @@ import React from 'react'
 
 import classes from './Order.module.css';
 
-function Order( props ) {
+const Order = ( props ) => {
+  
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return <span key={igKey} style={{
+      textTrasform: 'capitalise',
+      display: 'inline-block',
+      margin: '0 8px',
+      border: '1px solid #ccc',
+      padding: '5px'
+    }}>{igKey} ({props.ingredients[igKey]})</span>
+  })
+  
   return (
     <div className={classes.Order}>
-      <p>Ingredients: Meat (1)</p>
-      <p>Price: <strong>R 5.45</strong></p>
+      <p>Ingredients: {transformedIngredients}</p>
+      <p>Price: <strong>R {Number.parseFloat(props.price).toFixed(2)}</strong></p>
     </div>
   )
 }
