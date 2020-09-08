47cd6fcdc4627d08272c96f942e1d4eacbaa5cee
GrantW <Grant.Walker@ksdc.co.za>
2020-09-08 17:11:22 +0200

    Removed zero ingredients from orders display

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,18 +2,20 @@ import React from 'react'
 
 import classes from './Order.module.css';
 
-const Order = ( props ) => {
-  
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return <span key={igKey} style={{
-      textTrasform: 'capitalise',
-      display: 'inline-block',
-      margin: '0 8px',
-      border: '1px solid #ccc',
-      padding: '5px'
-    }}>{igKey} ({props.ingredients[igKey]})</span>
-  })
-  
+const Order = (props) => {
+
+  const transformedIngredients = Object.keys(props.ingredients)
+    .filter(igKey => props.ingredients[igKey] !== 0)
+    .map(igKey => {
+      return <span key={igKey} style={{
+        textTrasform: 'capitalise',
+        display: 'inline-block',
+        margin: '0 8px',
+        border: '1px solid #ccc',
+        padding: '5px'
+      }}>{igKey} ({props.ingredients[igKey]})</span>
+    })
+
   return (
     <div className={classes.Order}>
       <p>Ingredients: {transformedIngredients}</p>
