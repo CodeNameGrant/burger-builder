842b4fc62b9582f9a6620cd96bc83ae4c6c3ad03
GrantW <Grant.Walker@ksdc.co.za>
2020-09-09 21:54:55 +0200

    360; Added Container test coverage

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,8 +12,8 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
 import axios from '../../axios-orders';
 
-
-class BurgerBuilder extends Component {
+// Export Class to be used in test case
+export class BurgerBuilder extends Component {
   state = {
     purchasing: false,
   };
